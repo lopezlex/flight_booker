cea14528e695424dda2f3c3fa09362360115f66e
alexander lopez <alexander@bootyard.com>
2022-12-03 16:21:00 +0800

    add flight booker features

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  root 'flights#index'
+
+  resources :flights, only: [:index]
+  resources :bookings, only: [:new, :create, :show]
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
